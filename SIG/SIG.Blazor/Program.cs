3d86431c77117b2b8621168bf18d1aec54707e69
Thiago Luiz Silva <contato@thluiz.com>
2023-04-15 20:34:59 -0300

    Revert "fixing auth"
    This reverts commit 1590d29db34d1eb7b09ce3c84c066914fd6257ce.

--- SIG/SIG.Blazor/Program.cs
+++ SIG/SIG.Blazor/Program.cs
@@ -23,30 +23,6 @@ builder.Services.AddAuthorization(options =>
     options.FallbackPolicy = options.DefaultPolicy;
 });
 
-builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
-{
-    options.SaveTokens = true; // this saves the token for the downstream api
-    options.Events = new OpenIdConnectEvents
-    {
-        OnRedirectToIdentityProvider = async ctxt =>
-        {
-            // Invoked before redirecting to the identity provider to authenticate. This can be used to set ProtocolMessage.State
-            // that will be persisted through the authentication process. The ProtocolMessage can also be used to add or customize
-            // parameters sent to the identity provider.
-            ctxt.ProtocolMessage.RedirectUri = "https://sig.juliocamacho.org/signin-oidc";
-            await Task.Yield();
-        }
-    };
-});
-
-builder.Services.Configure<CookiePolicyOptions>(options =>
-{
-    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-    options.CheckConsentNeeded = context => true;
-    options.MinimumSameSitePolicy = SameSiteMode.None;
-    options.Secure = CookieSecurePolicy.Always;
-});
-
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor()
     .AddMicrosoftIdentityConsentHandler();
